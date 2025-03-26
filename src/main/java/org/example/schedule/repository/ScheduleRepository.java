00fcdb7babd4c918cfac961587b4b87b3722b2d7
Kim Sang Hyeon <kimjune.net@gmail.com>
2025-03-26 10:08:45 +0900

    Update ScheduleRepository.java

--- src/main/java/org/example/schedule/repository/ScheduleRepository.java
+++ src/main/java/org/example/schedule/repository/ScheduleRepository.java
@@ -12,7 +12,6 @@ public interface ScheduleRepository {
 
     List<ScheduleResponseDto> findAllSchedules();
 
-    /*Optional<Schedule> findScheduleById(Long id);*/
     Schedule findScheduleById(Long id);
 
 }
